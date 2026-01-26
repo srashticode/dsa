b01d4cc4f1aec041b925688d8b8a47fb86053b18
unknown <you@example.com>
2026-01-26 19:31:27 +0530

    sum of two number

---
+++ sum2.c
@@ -0,0 +1,14 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+    // Write C code here
+    int a =344 ;
+    int b =3434;
+    int c ;
+
+    c = a+b ;
+    printf("sum of two number%d",c);
+
+    return 0;
+}
