85676b64091dcc0aa72694133e74b57ab3a6799f
unknown <you@example.com>
2026-01-26 21:52:41 +0530

    addsubtract

---
+++ addsubtract.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main () {
+    int a ;
+    int b ;
+    int c ;
+    int d ;
+    printf("plz enter");
+    scanf("%d%d",&a ,&b);
+    c = a + b ;
+    printf("enter the number");
+    scanf("%d", &d);
+    c = c-d ;
+    return 0 ;
+}
