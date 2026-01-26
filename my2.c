42bec00f5d3c381721b8b007b6d49ebc0ab54104
unknown <you@example.com>
2026-01-26 19:36:44 +0530

    sagehttps://github.com/srashticode/dsa

--- my2.c
+++ my2.c
@@ -1,9 +1,9 @@
 // Online C compiler to run C program online
 #include <stdio.h>
 
-int main() {
+int main() {     
     // Write C code here
-    printf("Try programiz.pro");
+    printf("Try p chcangerogramiz.pro");
 
     return 0;
 }
