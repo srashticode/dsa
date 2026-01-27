8dd83a7dc4b2e63bc5beb29cf1e62d69e3cccdbd
unknown <you@example.com>
2026-01-27 23:56:11 +0530

    increementq1

---
+++ increementq1.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main(){
+    int a = 5;
+    printf("a = %d\n" ,a);
+    printf("a = %d\n" ,a++);
+    printf("a = %d\n" ,a);
+    printf("a = %d\n" ,a++);
+    printf("a = %d\n" ,a);
+        
+        return 0;
+    
+} 
