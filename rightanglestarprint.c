f418f7d1f18207712e2f74a2b71218e91cf304aa
unknown <you@example.com>
2026-02-04 23:32:40 +0530

    rightanglestarprint

---
+++ rightanglestarprint.c
@@ -0,0 +1,23 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+    int i,j;
+    for(i =1; i <=5; i++)
+    {
+        for(j =1; j <=i; j++)
+        {
+            printf("*");
+        }
+            printf("\n");
+        }
+            
+                return 0;
+}
+                
+            
+        
+    
+    
+
+    
