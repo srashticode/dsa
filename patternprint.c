54251c95f890353f85d2981c369d89032b401983
unknown <you@example.com>
2026-02-04 23:25:38 +0530

    patternprint

---
+++ patternprint.c
@@ -0,0 +1,23 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+    int i,j;
+    for(i =1; i <=4; i++)
+    {
+        for(j =1; j <=4; j++)
+        {
+            printf("*");
+        }
+            printf("\n");
+        }
+            
+                return 0;
+}
+                
+            
+        
+    
+    
+
+    
