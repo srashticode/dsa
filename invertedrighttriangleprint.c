43b1b17c6f2cd59a4e9500ea6c6850d8115e9884
unknown <you@example.com>
2026-02-05 23:15:42 +0530

    invertedrighttriangleprint

---
+++ invertedrighttriangleprint.c
@@ -0,0 +1,19 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+    int i,j;
+    for(i =1; i <= 4; i++) {
+        
+    for(j =4; j >= i ; j--) {
+        
+     printf("* ");
+         
+    }
+         
+         printf("\n");
+          
+    }
+         return 0 ;
+            
+    }
