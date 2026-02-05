d376f9a337b4faf3f2453b0f8d8683820825f2c9
unknown <you@example.com>
2026-02-05 22:59:00 +0530

    rightanglealignedprint

---
+++ rightanglealignedprint.c
@@ -0,0 +1,22 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+    int i,j;
+    for(i =1; i<= 4; i++) {
+       
+    //printing spaces
+        
+    for(j =1; j<=4 - i ; j++) {
+    printf("  ");
+    }
+          //   printing stars
+          for(j =1; j <=i; j++) {
+         printf(" *");
+}
+          printf("\n");
+          
+    }
+         return 0 ;
+            
+    }
