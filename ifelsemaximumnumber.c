5dc769e78b3458b0e0672fbc957316c72b633042
unknown <you@example.com>
2026-02-12 23:23:16 +0530

    ifelsemaximumnumber

---
+++ ifelsemaximumnumber.c
@@ -0,0 +1,18 @@
+ // C program to find maximum between two numbers
+
+#include <stdio.h>
+
+int main () {
+    int num1 , num2;
+    
+    printf("enter two numbers");
+    scanf("%d%d", &num1,&num2);
+    if(num1 > num2);
+    
+    printf("%d is maximum" ,num1);
+    
+    if(num1 == num2);
+    
+    return 0 ;
+    
+}
