ffddd68d2e3a5d98bdf66c48dae72fc61abdd009
unknown <you@example.com>
2026-02-13 21:58:45 +0530

    ifelsemaximumnumber2

---
+++ ifelsemaximumnumber2.c
@@ -0,0 +1,18 @@
+// write a program to find maximum between three numbers
+#include <stdio.h>
+
+int main() {
+    
+    int num1,num2,num3;
+    printf("enter three numbers");
+    scanf("%d%d%d", &num1, &num2, &num3);
+    
+    if("num1<num2<num3");
+    
+    printf("%d is maximum" ,num3);
+    if("num1==num2==num3");
+    
+    return 0;
+}
+    
+    
